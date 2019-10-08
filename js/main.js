b6c2ef6a187822a9927a6f3f0791864bf9e33f29
StasAlex <stas.alex28@gmail.com>
2019-10-08 19:27:00 +0300

    Сделан первый экран

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplaySpeed: 1500/true,
+      dots: false,
+      autoplayHoverPause: true
+    });
+  });
